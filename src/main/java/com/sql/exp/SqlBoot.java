8524ce8a03bd3f1da3ee8bff29ce414073995e6f
calcium <stephen.dery@gmail.com>
2022-08-14 16:23:30 -0400

    init sources and tests

---
+++ src/main/java/com/sql/exp/SqlBoot.java
@@ -0,0 +1,12 @@
+package com.sql.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlBoot.class, args);
+    }
+}
---
+++ src/test/java/com/sql/exp/TestHere.java
@@ -0,0 +1,12 @@
+package com.sql.exp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestHere {
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
