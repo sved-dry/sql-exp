703d0d697fb203121099bf02c8c7775013b82f95
sved-dry <stephen.dery@gmail.com>
2022-09-24 20:26:00 -0400

    add openapi spec

--- src/main/java/com/sql/exp/spring/SqlBoot.java
+++ src/main/java/com/sql/exp/spring/SqlBoot.java
@@ -1,5 +1,7 @@
 package com.sql.exp.spring;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = { "com.sql.exp" })
 @EnableJpaRepositories(basePackages = { "com.sql.exp" })
 @EntityScan(basePackages = { "com.sql.exp" })
+@OpenAPIDefinition(info = @Info(title = "stephen"))
 public class SqlBoot {
     private static final Logger logger = LoggerFactory.getLogger(SqlBoot.class);
 
